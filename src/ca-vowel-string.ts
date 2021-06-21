f83146d6039e02328d3a2c98cd0cbeecd8752197
David McGill <david.mcgill@optum.com>
2021-06-21 14:21:28 -0500

    add objective and base code

---
+++ src/ca-vowel-string.ts
@@ -0,0 +1,19 @@
+/** This is our standard code assessment. 
+ * Please take the time to complete it, and share it with us however you deem appropriate.
+
+In the programming language of your choice, complete the following objective:
+
+Given a string, count the number of unique vowels in each word, then remove all but the first and last
+letters in each word. Place the number of unique values in between the remaining letters, then output
+the full string. Leave any characters that are not alpha-numeric as they are.
+
+Example: “The stray cat was cool.”
+Becomes: “T1e s2y c1t w1s c1l.”
+
+Treat this project as if it was code that you are going to deploy to a production environment.  
+This exercise should take around an hour.   Thanks, and I look forward to getting to know you! */
+
+
+let example: string = 'The stray cat was cool.';
+console.log(example);
+
